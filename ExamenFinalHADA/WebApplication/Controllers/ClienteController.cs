265117ae109be0b4391a67990c89aad9df9bda22
LastKing <LastKing@DESKTOP-OOM640H>
2020-03-14 12:32:20 -0500

    Luchandola Profe :v

---
+++ ExamenFinalHADA/WebApplication/Controllers/ClienteController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication.Models.BEAN;
+using WebApplication.Models.Conection;
+
+namespace WebApplication.Controllers
+{
+    public class ClienteController : Controller
+    {
+        // GET: Cliente
+        public ActionResult Index()
+        {
+            try
+            {
+                List<tbCliente> listaCliente;
+                using (var db = new BD_ExamenFinalEntities())
+                {
+                    listaCliente = db.tbCliente.ToList();
+                    return View(listaCliente);
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            
+        }
+    }
+}
---
+++ ExamenFinalHADA/WebApplication/Models/BEAN/ClienteBEAN.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication.Models.BEAN
+{
+    public class ClienteBEAN
+    {
+        public int IdCliente { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public int NumDocumento { get; set; }
+    }
+}
